dc31815974b49b7badecadcc2e46d7bf09d6c4e6
masashi-seki <106162510+masashi-seki@users.noreply.github.com>
2022-11-10 16:16:55 +0900

    Add files via upload

---
+++ デザイン案トップ/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+    $(".slider").slick({
+        Infinite:true,
+        slidestoshow:1,
+        slidestoscroll:1,
+        arrows:true,
+        dots:true,
+        autoplay:false,
+    });
+});
+$(function () {
+    $(".slider2").slick({
+        Infinite:true,
+        slidestoshow:1,
+        slidestoscroll:1,
+        arrows:true,
+        dots:true,
+        autoplay:false,
+    });
+});
+$(function () {
+    $(".slider3").slick({
+        Infinite:true,
+        slidestoshow:1,
+        slidestoscroll:1,
+        arrows:true,
+        dots:true,
+        autoplay:false,
+    });
+});
